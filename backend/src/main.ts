c4e63b22a08210933a01198606469468f220a292
Carlos Veríssimo <94475392+carlosverissimo3001@users.noreply.github.com>
2026-02-05 18:02:16 +0000

    fix: swagger + remove unused code paths (#14)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,35 +16,12 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  const config = new DocumentBuilder()
-    .setTitle('Unpaused Spotify API')
-    .setDescription('The backend for the Spotify guessing game')
-    .setVersion('1.0')
-    .addCookieAuth('unpaused_session', {
-      type: 'apiKey',
-      in: 'cookie',
-      name: SESSION_COOKIE_NAME
-    })
-    .build();
-
-    const document = SwaggerModule.createDocument(app, config, {
-      operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
-    });
-
-  fs.writeFileSync(
-    // Note: This is ugly af
-    path.join(__dirname, '../../..', 'swagger-spec.json'),
-    JSON.stringify(document, null, 2),
-  );
-
   const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
   app.enableCors({
     origin: frontendUrl,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  SwaggerModule.setup('docs', app, document);
 
   app.useGlobalPipes(VALIDATION_CONFIG);
   await app.listen(process.env.PORT ?? 3001);
